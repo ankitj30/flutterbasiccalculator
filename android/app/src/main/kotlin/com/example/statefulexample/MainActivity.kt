091d4d56b22b13a406bb20084c53f92f0773bbf8
Ankit Jaiswal <ankitjaiswal3016@gmail.com>
2025-06-18 00:39:54 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/statefulexample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.statefulexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp()); // make this const
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // keep const
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(), // <-- also make this const
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key}); // <-- add const here
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var no1controller = TextEditingController();
+  var no2controller = TextEditingController();
+  var result = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Basic Calculator')),
+      body: Container(
+        color: Colors.blue.shade100,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                controller: no1controller,
+                ),
+                TextField(
+                controller: no2controller,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1controller.text.toString());
+                        var no2 = int.parse(no1controller.text.toString());
+                        var sum = no1+no2;
+                        result = " The sum $no1 and $no2 is $sum";
+                        setState(() {});
+                      }, child: Text('add')),
+
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1controller.text.toString());
+                        var no2 = int.parse(no1controller.text.toString());
+                        var diff = no1-no2;
+                        result = " The sub $no1 and $no2 is $diff";
+                        setState(() {});
+                      }, child: Text('sub')),
+
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1controller.text.toString());
+                        var no2 = int.parse(no1controller.text.toString());
+                        var mul = no1*no2;
+                        result = " The mul $no1 and $no2 is $mul";
+                        setState(() {});
+                      }, child: Text('mul')),
+
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1controller.text.toString());
+                        var no2 = int.parse(no2controller.text.toString());
+                        var div = no1/no2;
+                        result = " The div $no1 and $no2 is ${div.toStringAsFixed(3)}";
+                        setState(() {});
+                      }, child: Text('div')),
+                    ],
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(21),
+                  child: Text(result,style: TextStyle(fontSize: 25,color: Colors.black87),),),
+              ],
+
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
